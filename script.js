410eadf346efb945318c985ab915207ce3de877b
prajwal mhaskar <33192878+PRAJWALMHASKAR@users.noreply.github.com>
2024-09-18 01:41:34 +0530

    Updated script.js - added Table Links
    Added table links to perform operations on the table using javascript.

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ const changebgred = ()=>{
 
 let b = document.body
 
-console.log("First Element child of b is: ", b.firstElementChild)
-console.log("First child of b is: ", b.firstChild)
+//console.log("First Element child of b is: ", b.firstElementChild)
+//console.log("First child of b is: ", b.firstChild)
+
+let t = document.body.firstElementChild.nextElementSibling.firstElementChild;
+
+console.log(t);
+console.log(t.rows);
+console.log(t.caption);
+console.log(t.tHead);
+console.log(t.tBodies);
+//console.log(tbody.rows);//error->will resolve this
