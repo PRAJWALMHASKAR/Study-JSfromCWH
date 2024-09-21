d2e38a2f36d89b3e7d9f0f049df3f9b95daf58dd
Bunny <prajwalmhaskar28@gmail.com>
2024-09-21 19:28:20 +0530

    Resolved previous errors such as, DOM manupulation (reaching to tbody)

--- script.js
+++ script.js
@@ -14,4 +14,11 @@ console.log(t.rows);
 console.log(t.caption);
 console.log(t.tHead);
 console.log(t.tBodies);
+
 //console.log(tbody.rows);//error->will resolve this
+
+console.log(t.tBodies[0].rows) //=>tbody.rows // Instead of tbody.rows, we have to use t.rows because the DOM recognizes rows from table, there is no need to call tbody (anyhow it will show error unless it is defined in js document)
+
+console.log(t.rows[0].cells); //=>tr.cells
+
+//console.log(t.rows[1].cellIndex);
